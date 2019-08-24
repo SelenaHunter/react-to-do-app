5afa8c7c4e2a1ec0c19ff23aef53f975d0d70992
Selena Hunter <selenahunter99@gmail.com>
2019-08-24 18:51:34 -0500

    Updates

---
+++ precache-manifest.d21f3a1caf104db88eddab54a1f89b2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20846e8ca56f174daa256a38b8714eae",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "ae5e0e351f383bd049b5",
+    "url": "/react-to-do-app/static/css/main.6f740971.chunk.css"
+  },
+  {
+    "revision": "53ca71971cb112fcd7a8",
+    "url": "/react-to-do-app/static/js/2.44cdfe43.chunk.js"
+  },
+  {
+    "revision": "ae5e0e351f383bd049b5",
+    "url": "/react-to-do-app/static/js/main.9df309a0.chunk.js"
+  },
+  {
+    "revision": "e93524d006b93b689cb2",
+    "url": "/react-to-do-app/static/js/runtime~main.ab6653ee.js"
+  }
+]);
